3f3a0be5b118d6a67d5d2ff85a64165f151f1fe4
윤주형 <juhyung0818@naver.com>
2016-11-29 12:05:45 +0900

    update cafe list

--- src/main/java/com/cafe/controller/CafeController.java
+++ src/main/java/com/cafe/controller/CafeController.java
@@ -10,8 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cafe.domain.CafeVO;
+import com.cafe.domain.DetailVO;
+import com.cafe.domain.MenuVO;
+import com.cafe.domain.SearchKeywordVO;
 import com.cafe.service.CafeService;
 
 /**
@@ -37,10 +42,53 @@ public class CafeController {
 	 * @throws Exception
 	 */
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public void cafeListGET(Model model) throws Exception {
+	public void cafeListGET(@RequestParam("keyword") String keyword, Model model) throws Exception {
+		
 		logger.info("Cafeteria list....");
 		
-		List<CafeVO> cafes= cafeService.cafeList();
+		List<CafeVO> cafes = cafeService.cafeSearch(keyword);
 		model.addAttribute("cafes", cafes);
+		model.addAttribute("keyword", keyword);
+
 	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String menuRegisterPOST(@RequestParam("cafeName") String cafeName, 
+			Model model, SearchKeywordVO key, RedirectAttributes rttr) throws Exception {
+		
+		logger.info("Cafeteria register....");
+		
+		CafeVO cafe = new CafeVO();
+		cafe.setCafeName(cafeName);
+		cafe.setInfo("");
+		cafeService.cafeRegister(cafe);
+		rttr.addAttribute("cafeName", cafeName);
+		rttr.addAttribute("keyword", key.getKeyword());
+		
+		return "redirect:/cafe/list";
+	}
+
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	public String menuDeletePOST(@RequestParam("cafeName") String cafeName, 
+			Model model, SearchKeywordVO key, RedirectAttributes rttr) throws Exception {
+		
+		logger.info("Cafeteria delete....");
+		
+		cafeService.deleteCafe(cafeName);
+		rttr.addAttribute("cafeName", cafeName);
+		rttr.addAttribute("keyword", key.getKeyword());
+		
+		return "redirect:/cafe/list";
+	}
+
+	@RequestMapping(value = "/search", method = RequestMethod.POST)
+	public String menuSearhPOST(@RequestParam("keyword") String keyword,
+			Model model, RedirectAttributes rttr) throws Exception {
+		
+		logger.info("Cafe search....");
+		rttr.addAttribute("keyword", keyword);
+		
+		return "redirect:/cafe/list";
+	}
+
 }
