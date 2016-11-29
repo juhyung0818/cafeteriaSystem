a18ad5da0b2747f215f5cb7007296a86d9d33067
윤주형 <juhyung0818@naver.com>
2016-11-29 14:20:55 +0900

    update weeklyList

--- src/main/java/com/cafe/controller/WeeklyController.java
+++ src/main/java/com/cafe/controller/WeeklyController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.cafe.domain.MenuVO;
 import com.cafe.domain.ResultVO;
 import com.cafe.domain.WeeklyVO;
 import com.cafe.flag.DateFlag;
